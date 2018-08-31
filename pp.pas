20c11a2edec3873151ac7a2749b5ad6d4a18cd1a
bentokun <fewdspuckrito@gmail.com>
2018-08-31 13:13:45 +0700

    Add support for Themis 1.7.6.744
    - Do a check for version to decide which pointer address should be used
    - Find and add pointer for Themis 1.7.6.744

--- pp.pas
+++ pp.pas
@@ -22,7 +22,9 @@
     SOFTWARE.
 }
 
-uses windows;
+{$MODE OBJFPC}
+
+uses windows, sysutils;
 
 const
   kernel32 = 'kernel32.dll';
@@ -57,17 +59,84 @@ const
 function CreateToolhelp32Snapshot(dwFlags, th32ProcessID: DWORD): HANDLE; stdcall; external kernel32 name 'CreateToolhelp32Snapshot';
 function Process32Next(hSnapshot: HANDLE; var lppe: PROCESSENTRY32): BOOL; stdcall; external kernel32 name 'Process32Next';
 function Process32First(hSnapshot: HANDLE; var lppe: PROCESSENTRY32): BOOL; stdcall; external kernel32 name 'Process32First';
+function QueryFullProcessImageNameA(hProcess: HANDLE; dwFlags: DWORD; lpExeName: PChar; var dwsize: DWORD): BOOL; stdcall; external kernel32 name 'QueryFullProcessImageNameA';
 
+const currentTestFolderPtr1 = $18BC10;  // 1.7.6.144
+      currentTestFolderPtr2 = $12BF58; // 1.9.6.2331
+      target = 'bai1';
 
-const currentTestFolderPtr = $12BF58;
-      target = 'BAI1';
+type
+   themisVersion = record
+      major, minor, build, revision: DWORD;
+   end;
+
+function VersionIdentical(ver: themisversion; major, minor, build, revision: DWORD): boolean;
+begin
+    if ((ver.major = major) and (ver.minor = minor) and (ver.build = build) and (ver.revision = revision)) then
+       exit(true);
+
+    exit(false);
+end;
+
+function GetThemisTestPointerFromver(ver: themisVersion): DWORD;
+begin
+    // This address is not stable
+    if VersionIdentical(ver, 1, 7, 6, 744) then
+       exit(currentTestFolderPtr1);
+
+    if (VersionIdentical(ver, 1, 9, 6, 2331)) then
+       exit(currentTestFolderPtr2);
+
+    exit(0);
+end;
+
+function GetThemisVersion(path: AnsiString): themisVersion;
+type FILEINFOPTR = ^VS_FIXEDFILEINFO;
+var verSize, verHandle: DWORD;
+    puLenFileInfo: DWORD;
+    verData: array of BYTE;
+    fileInfoP: FILEINFOPTR;
+    fileInfo: VS_FIXEDFILEINFO;
+begin
+        versize := GetFileVersionInfoSize(@path[1], verHandle);
+
+        if (versize > 0) then
+        begin
+            SetLength(verData, verSize);
+
+            if GetFileVersionInfo(@path[1], verHandle, verSize, @verData[0]) then
+            begin
+                if (VerQueryValue(@verData[0], '\', @fileInfoP, @puLenFileInfo)) then
+                begin
+                    fileInfo := fileInfoP^;
+
+                    if (puLenFileInfo > 0) and (fileInfo.dwSignature = $feef04bd)
+                    then
+                    begin
+                        GetThemisversion.major := (fileInfo.dwFileversionMS shr 16) and $FFFF;
+                        GetThemisversion.minor := (fileInfo.dwFileversionMS shr 0) and $FFFF;
+                        GetThemisversion.build := (fileInfo.dwFileversionLS shr 16) and $FFFF;
+                        GetThemisversion.revision := (fileInfo.dwFileversionLS shr 0) and $FFFF;
+
+                        exit;
+                   end;
+                end;
+            end;
+        end;
+
+        raise Exception.Create('Cannot get the version of Themis');
+end;
+
 
 function GetThemisCurrentTest(var path: UnicodeString): boolean;
 var entry: TProcessEntry32;
     snapshot, themisprocess: THandle;
     pathReadSucc, found: boolean;
     buffer: packed array[0..300] of char;
-    bytesRead: DWord;
+    bytesRead, strsize, tfp: DWord;
+    ver: ThemisVersion;
+    fullPath: AnsiString;
+    fullPathBuf: packed array[0..1000] of char;
 begin
     found:=false;
 
@@ -78,14 +147,22 @@ begin
     begin
         while (Process32Next(snapshot, entry) = TRUE) do
         begin
-            writeln(AnsiString(entry.szExeFile));
-
             if (AnsiString(entry.szExeFile) =  'Themis.exe')  then
             begin
                 themisprocess := OpenProcess(PROCESS_ALL_ACCESS,
                     FALSE, entry.th32ProcessID);
 
-                pathReadSucc := ReadProcessMemory(themisprocess, Pointer(currentTestFolderPtr),
+                strsize:=1000;
+
+                if (QueryFullProcessImageNameA(themisprocess, 0,@fullPathBuf[0],
+                        strsize)) then
+                     fullPath := AnsiString(fullPathBuf);
+
+                ver := GetThemisVersion(fullPath);
+
+                tfp:=GetThemisTestPointerFromVer(ver);
+
+                pathReadSucc := ReadProcessMemory(themisprocess, Pointer(tfp),
                     @buffer, 200, bytesRead);
 
                 if (pathReadSucc) then
